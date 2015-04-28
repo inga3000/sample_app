46422fd4535e16f5b4e55ca46d8b8e41351ce6b0
Inga <inga.wiegand@gmail.com>
2015-04-28 20:49:21 +0200

    Added new controller action and created controller (33)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150426202941) do
+ActiveRecord::Schema.define(version: 20150428181229) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150426202941) do
     t.datetime "updated_at",  null: false
     t.string   "colour"
     t.string   "color"
+    t.integer  "price"
   end
 
 end
