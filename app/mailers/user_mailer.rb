d5eb7ac181f95ece14f84514f9513ad6f0d9dc9c
Inga <inga.wiegand@gmail.com>
2015-05-30 11:52:18 +0200

    Styled contact form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "inga.wiegand@gmail.com'"
 
   def contact_form(email, name, message)
 	@message = message
   	mail(:from => email, 
-	  		:to => 'your-email@example.com', 
+	  		:to => 'inga.wiegand@gmail.com', 
 	  		:subject => "A new contact form message from #{name}")
   end
 
