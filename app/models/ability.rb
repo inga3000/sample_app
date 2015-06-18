4d03fef7358ba3eb706f6ccfe839c890d6cb3853
Inga <inga.wiegand@gmail.com>
2015-06-18 21:31:54 +0200

    Added delete funtion to comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,8 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
+    if user && user.admin?
+      can [:update,:destroy], :all
   end
 
 end
