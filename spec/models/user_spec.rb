dd0700f54413dc4e47644e0fd67d5f7bb1efd9b1
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2015-06-25 10:00:47 +0300

    TESTs for user model have been rewritten

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,34 +8,56 @@ require "rails_helper"
 				before do
 					subject.email = "user@example.com"
 					subject.password = "qwerty1234"
+
+          expect(subject.password).to_not match(/[A-Z]/)
 				end
 
 				it 'should not be valid ' do
-					expect(subject.errors[:password]).to include( )
+          # fire
+          subject.valid?
+
+					# check that the test gives the result that we want
+					expect(subject.errors[:password]).to include("must include at least one lowercase letter, one uppercase letter, and one digit")
 				end
 			end
 
-			context 'when is too short' do
+			context 'when it has length less than 8' do
 				before do
 					subject.email = "user@example.com"
 					subject.password ="abc"
+
+          expect(subject.password.length).to be < 8
 				end
 
 				it 'should not be valid ' do
-					expect(subject.errors[:password]).to include( )
+          # fire
+          subject.valid?
+
+          # check that the test gives the result that we want
+					expect(subject.errors[:password]).to include("is too short (minimum is 8 characters)")
 				end
 			end
 
 			context 'when password confirmation does not match' do
 				before do
-					subject.email = "user@example.com", 
+					subject.email = "foo@mailinator.com"
 					subject.password ="CareerFoundry1"
 					subject.password_confirmation = "CareerFoundry2"
+
+          expect(subject.password).to_not eq(subject.password_confirmation)
 				end
 
 				it 'should not be valid ' do
-					expect(subject.errors[:password]).to include( )
-				end 
+          # fire
+          expect do
+            begin
+              subject.save!
+            rescue Exception => ex
+              expect(ex.message).to include("Validation failed: Password confirmation doesn't match Password")
+              raise
+            end
+          end.to raise_error(ActiveRecord::RecordInvalid)
+				end
 			end
 
 		end
