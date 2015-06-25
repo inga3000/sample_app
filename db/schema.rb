4c3f42ad931058d3143e65838026a9319b067c50
Panayotis Matsinopoulos <panayotis@matsinopoulos.gr>
2015-06-25 09:48:04 +0300

    Addd update db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -25,12 +25,6 @@ ActiveRecord::Schema.define(version: 20150618185125) do
   add_index "comments", ["product_id"], name: "index_comments_on_product_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
 
-  create_table "first_names", force: :cascade do |t|
-    t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
     t.integer "product_id"
