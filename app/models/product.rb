6be9498bdcd1d5397b4c04fa06c24cc6ad6f0793
Inga <inga.wiegand@gmail.com>
2015-08-02 11:51:36 +0200

    Debugging Paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ActiveRecord::Base
   has_many :orders
   has_many :comments
   has_attached_file :photo
+  attr_accessor :photo_file_name
+
   validates :name, presence: true
 
   def average_rating
