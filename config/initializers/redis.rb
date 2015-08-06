a2abe71dde2d496cd341748f12728e5236c7d4c0
Inga <inga.wiegand@gmail.com>
2015-08-06 19:42:39 +0200

    Preparing Redis for Heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,8 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env.development? || Rails.env.test?
+  $redis = Redis.new(:host => 'localhost', :port => 6379)
+elsif Rails.env.production?
+  $redis = Redis.new(host: 'pub-redis-11935.us-east-1-3.6.ec2.redislabs.com',
+  	                 port: 11935)	
+end  
+
+#redis://rediscloud:obufgVlN5oRHD8p1@pub-redis-11935.us-east-1-3.6.ec2.redislabs.com:11935
